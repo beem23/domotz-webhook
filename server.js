b3cdbd336bf44f9ceb3526eb25ee4614ad6596bb
Marlon Bellot <marlonbellot4@gmail.com>
2024-07-11 16:31:34 -0400

    changed the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
